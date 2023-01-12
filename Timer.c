bd3388ca7aab67d5ed90049e34920efc5855423d
gmaximo13 <gustavo.maximo80@gmail.com>
2023-01-12 01:29:02 -0300

    Timer PIC18F452

--- Timer.c
+++ Timer.c
@@ -1,20 +1,18 @@
 /* ****************************************************************
-2** Exemplo 05 **
-3** **
-**Exemplo para utilizacao do display de 7 segmentos **
-** presente no kit PIC 18 F452 . Cada um dos 4 displays **
-** sao acessos , um de cada , vez , exibindo seu indice **
-** Ex: display 1 = 1 , display 2 = 2 , etc ... **
+** Atividade 05 **
+** Gustavo dos Santos Moreira Máximo **
+** Matricula: 201820512 **
 ** **
-** Arquivo : display7seg .c **
+** Código para a implementação de um timer **
+** Utilizando o microcontrolador PIC18F452 **
+** E 4 displays de 7 segmentos **
+** **
 ** Compilador : MikroC PRO PIC v .6.4.0 **
 ** **
-** Obs: Ativar dips switchs : DISP1 a DISP4 **
 ** **
-** UFLA - Lavras /MG - 12/07/2017 **
+** UFLA - Lavras /MG - 12/01/2023 **
 ***************************************************************** */
-
- // Define o tempo de acendimento do display em ms.
+ // Define o tempo para a multiplexação dos displays.
  # define tempo 5
 
  // Converte valor numerico decimal para codigo 7 segmentos
@@ -34,75 +32,100 @@
    }
  }
 
+//Função responsavel por acionar os displays
+//mostrando os segungos ao usuario.
+
 void exibicao(int segundos){
 
     PORTA = 0;
-
-  //  if(segundos > 999){
-      PORTD = mask((segundos/1000)%10) ;
-      porta.f2 = 1; // Ativa display 1.
-      Delay_ms (tempo) ;
-      porta.f2 = 0; // Desativa display 1.
-    //}
+    PORTD = mask((segundos/1000)%10) ;
+    porta.f2 = 1; // Ativa display 1.
+    Delay_ms (tempo) ;
+    porta.f2 = 0; // Desativa display 1.
     
-    //if(segundos > 99){
-      PORTD = mask((segundos/100)%10);
-      porta.f3 = 1; // Ativa display 2.
-      Delay_ms ( tempo );
-      porta.f3 = 0; // Desativa display 2.
-    //}
-    
-    //if(segundos > 9){
-      PORTD = mask((segundos/10)%10);
-      porta.f4 = 1; // Ativa display 3.
-      Delay_ms ( tempo );
-      porta.f4 = 0; // Desativa display 3.
-    //}
-
-    PORTD = mask(segundos%10);
-    porta.f5 = 1; // Ativa display 4.
+    PORTD = mask((segundos/100)%10);
+    porta.f3 = 1; // Ativa display 2.
     Delay_ms ( tempo );
-    porta.f5 = 0; // desativa display 4.
+    porta.f3 = 0; // Desativa display 2.
+    
+
+     PORTD = mask((segundos/10)%10);
+     porta.f4 = 1; // Ativa display 3.
+     Delay_ms ( tempo );
+     porta.f4 = 0; // Desativa display 3.
+
+     PORTD = mask(segundos%10);
+     porta.f5 = 1; // Ativa display 4.
+     Delay_ms ( tempo );
+     porta.f5 = 0; // desativa display 4.
 
 }
 
+//Função responsavel por acionar o Buzzer
+//O buzzer por 10 segundos ou até que o botão rb2 seja acionado
+
+void acionaBuzzer(int segundos){
+       int i = 0;
+       while((portb.rb2 != 0) && (i<10)){
+           exibicao(segundos);
+           portc.rc1 = 1;
+           delay_ms(500);
+           exibicao(segundos);
+           portc.rc1 = 0;
+           delay_ms(500);
+           i++;
+       }
+}
 void main(){
   
   int segundos = 0;
   
   ADCON0 = 0X00 ;
   ADCON1 = 0X06 ; // desabilita conversor A/D.
-  INTCON = 0; // desabilita interrupcoes .
-  TRISA = 0; // configura portA como saida .
+  INTCON = 0; // desabilita interrupcoes.
+  TRISA = 0; // configura portA como saida.
   PORTA = 0;
-  TRISD = 0; // configura portD como saida .
+  TRISD = 0; // configura portD como saida.
   PORTD = 0;
-  TRISB = 7; // Configura os pinos rb0, rb1 e rb2 como entrada
+  TRISB = 7; // Configura os pinos rb0, rb1 e rb2 como entrada.
+  trisc = 0xFD; //Configura o pino rc1 como saída.
   
-  while (1){ // inicio do loop infinito .
+  portc.rc1 = 0; //Inicia o buzzer desligado
+  
+  while (1){ // inicio do loop infinito.
     
     exibicao(segundos);
     
-    if(segundos >= 0 && segundos < 9999){
-      if(portb.rb0 == 0){
+    if(segundos >= 0 && segundos < 9999){ // Condição de limite do timer
+      
+      if(portb.rb0 == 0){ //Incrementa segundos quando o botão rb0 é acionado
           segundos++;
           exibicao(segundos);
-          delay_ms(50);
       }
 
-      if(portb.rb1 == 0){
-         segundos--;
+      if(portb.rb1 == 0){   //Decrementa segundos quando o botão rb1 é acionado
+         if(segundos != 0){ //Impede que a variavel segundo seja menor que zero
+             segundos--;
+         }
          exibicao(segundos);
       }
 
-      if(portb.rb2 == 0){
+      if(portb.rb2 == 0){  //Aciona o timer quando o botão rb2 é pressionado
          while(segundos != 0){
+             int i;
              segundos--;
-             exibicao(segundos);
-             Delay_ms(1000);
+             
+             //Para cada chamada da função exibição são gastados 20ms por conta
+             //da multiplexação, logo 50*20 = 1000ms (1 segundo).
+             //Essa estrutura de repetição garante que todos os displays se manteram ativos
+             //durante a contagem regressiva.
+             
+             for(i=0; i<=50; i++){
+                 exibicao(segundos);
+             }
          }
+         acionaBuzzer(segundos);
       }
     }
-  
   }
 }
